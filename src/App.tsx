bafb32d30f8585b4041f4aa11873ead77179bcde
Carlos Cruz <ceducruz@gmail.com>
2021-06-27 22:42:25 -0300

    Aula 3 - Parte 2, Aula 4 e 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import { NewRoom } from './pages/NewRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-        </Switch>
-      </AuthContextProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AuthContextProvider>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/rooms/new" component={NewRoom} />
+                    <Route path="/rooms/:id" component={Room} />
+
+                    <Route path="/admin/rooms/:id" component={AdminRoom} />
+                </Switch>
+            </AuthContextProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
